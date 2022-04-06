4225e40a45765bbfd55906e95c1dc438bbec2abd
ericminio <eric.mignot@gmail.com>
2022-04-06 07:09:00 -0700

    extract page

--- about/1.display-hints.spec.js
+++ about/1.display-hints.spec.js
@@ -1,68 +1,33 @@
 const { expect } = require('chai');
-const { JSDOM } = require('jsdom');
-const http = require('http');
-const port = 5001;
-const fs = require('fs');
-const assets = {
-    '/': { 
-        contentType: 'text/html', 
-        content:fs.readFileSync('../app/index.html').toString() 
-    },
-    '/poh.js': { 
-        contentType: 'application/javascript', 
-        content:fs.readFileSync('../app/poh.js').toString() 
-    },
-    '/format-time.js': { 
-        contentType: 'application/javascript', 
-        content:fs.readFileSync('../app/format-time.js').toString() 
-    }
-};
+const { page } = require('./page');
 
 describe('display hints when choosing a step', () => {
 
-    let server;
-    let document;
     let hints;
     beforeEach((done) => {
-        server = http.createServer((request, response) => {
-            response.setHeader('Content-Length', assets[request.url].content.length);
-            response.setHeader('Content-Type', assets[request.url].contentType);
-            response.write(assets[request.url].content);
-            response.end();
-        });
-        server.listen(port, () => {
-            JSDOM.fromURL(`http://localhost:${port}`, { 
-                runScripts: "dangerously", 
-                resources: "usable" 
-            }).then(dom => {                
-                document = dom.window.document;
-                hints = document.getElementById('hints');
-                document.addEventListener('DOMContentLoaded', () => {                    
-                    done();
-                });                
-            }).catch(error => {
-                done(error);
-            });
+        page.open(() => {
+            hints = page.document.getElementById('hints');
+            done();
         })
     });
     afterEach(() => {
-        server.close();
+        page.close();
     })
 
     it('works with step test', () => {        
-        let step = document.getElementById('test');
+        let step = page.document.getElementById('test');
         step.click();
 
         expect(hints.innerHTML).to.contain('go to red');
     });
     it('works with step code', () => {
-        let step = document.getElementById('code');
+        let step = page.document.getElementById('code');
         step.click();
 
         expect(hints.innerHTML).to.contain('go to green');
     });
     it('works with step refactor', () => {
-        let step = document.getElementById('refactor');
+        let step = page.document.getElementById('refactor');
         step.click();
 
         expect(hints.innerHTML).to.contain('duplication?');
