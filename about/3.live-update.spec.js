3b84cc8399a4a2b0aa0a79bbc3df55fa163d850b
ericminio <eric.mignot@gmail.com>
2022-04-06 07:23:00 -0700

    live update

---
+++ about/3.live-update.spec.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const { page } = require('./page');
+
+describe('live update', () => {
+
+    beforeEach((done) => {
+        page.open(done)
+    });
+    afterEach(() => {
+        page.close();
+    })
+
+    it('is triggered via start', (done) => {
+        page.window.delay = 10;
+        page.document.getElementById('start').click();
+        setTimeout(() => {
+            expect(page.window.times).to.deep.equal({
+                test: 2, code: 0, refactor: 0,
+                ptest: 1, pcode: 0, prefactor: 0
+            });
+            done();
+        }, 30);
+    });
+
+    it('will stop after delay when interupted', (done) => {
+        page.window.delay = 10;
+        page.document.getElementById('start').click();
+        setTimeout(() => {
+            page.document.getElementById('stop').click();
+            setTimeout(() => {
+                expect(page.window.times).to.deep.equal({
+                    test: 3, code: 0, refactor: 0,
+                    ptest: 1, pcode: 0, prefactor: 0
+                });
+                done();
+            }, 30);
+        }, 30);
+    });
+});
