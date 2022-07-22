d7fe958d187ded8df413adb27788c4805d26d80b
ericminio <eric.mignot@gmail.com>
2022-07-22 07:51:40 -0700

    use querySelector

--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
 var eventBus = new EventBus();
-new HintsRenderer({eventBus, procedures, placeholder: document.getElementById('hints')});
+new HintsRenderer({eventBus, procedures, placeholder: document.querySelector('#hints')});
 
-document.getElementById('test').addEventListener('click', () => {        
+document.querySelector('#test').addEventListener('click', () => {        
     eventBus.notify('step', 'test');
 });
-document.getElementById('code').addEventListener('click', () => {        
+document.querySelector('#code').addEventListener('click', () => {        
     eventBus.notify('step', 'code');
 });
-document.getElementById('refactor').addEventListener('click', () => {        
+document.querySelector('#refactor').addEventListener('click', () => {        
     eventBus.notify('step', 'refactor');
 });
 
-document.getElementById('test').click();
+document.querySelector('#test').click();
